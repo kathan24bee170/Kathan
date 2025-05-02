4d0944e3ee746a4c4fe18a3b5caa9fab36687274
kathan24bee170 <24bee170@spt.pdpu.ac.in>
2025-05-02 11:13:09 +0530

    Add files via upload

---
+++ Q13.py
@@ -0,0 +1,7 @@
+Byte=int(input("enter number of bytes: "))
+KB=Byte/1024
+MB=KB/1024
+GB=MB/1024
+print("Entered amount of Kilobytes: ",KB)
+print("Entered amount of Megabytes: ",MB)
+print("Entered amount of Gigaytes: ",GB)
